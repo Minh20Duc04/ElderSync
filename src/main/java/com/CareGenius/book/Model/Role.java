5aa1b46bba3395b96a8782eac00e65cc8e116907
Minh20Duc04 <nguyenminhduc02042004@gmail.com>
2025-07-24 16:42:20 +0700

    Week 1 _ Creating project and models

---
+++ src/main/java/com/CareGenius/book/Model/Role.java
@@ -0,0 +1,7 @@
+package com.CareGenius.book.Model;
+
+public enum Role {
+    SEEKER,
+    GIVER,
+    ADMIN
+}
---
+++ src/main/java/com/CareGenius/book/Model/User.java
@@ -0,0 +1,73 @@
+package com.CareGenius.book.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "users")
+
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uid;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "user_role")
+    private Role role;
+
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    private LocalDate updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
